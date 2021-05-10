fb99cc3f1e49c39fb8ea355debc2a3a8db271b6d
Estelle Recuero <estelle-ad@outlook.com>
2021-05-11 01:11:37 +0200

    update path

--- ft_server/srcs/index_switch.sh
+++ ft_server/srcs/index_switch.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if ! cmp -s /etc/nginx/sites-enabled/localhost tmp/nginx_config_off
+if ! cmp -s /etc/nginx/sites-enabled/localhost /tmp/nginx_config_off
 then
 	echo "index OFF"
 	cp /tmp/nginx_config_off /etc/nginx/sites-available/localhost
 	cp /var/www/html/index.nginx-debian.html /var/www/localhost/index.html
 	rm /etc/nginx/sites-enabled/localhost
 	ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
-elif ! cmp -s /etc/nginx/sites-enabled/localhost tmp/nginx_config_on
+elif ! cmp -s /etc/nginx/sites-enabled/localhost /tmp/nginx_config_on
 then
 	echo "index ON"
 	rm /var/www/localhost/index.html
